2dedc1cc38d6275ec72ac56a8b7492a953ae9ab1
Payal Bannagare <payalbannagare585@gmail.com>
2023-01-17 11:13:12 +0530

    Add project files.

---
+++ IODemo/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace IODemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnBinaryWrite = new System.Windows.Forms.Button();
+            this.btnBinaryRead = new System.Windows.Forms.Button();
+            this.btnCreateFolder = new System.Windows.Forms.Button();
+            this.btnCreateFile = new System.Windows.Forms.Button();
+            this.btnDeleteFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(96, 55);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(140, 20);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Enter Employee Id";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(96, 144);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(168, 20);
+            this.lbl2.TabIndex = 1;
+            this.lbl2.Text = "Enter Employee Name";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(96, 215);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(139, 20);
+            this.lbl3.TabIndex = 2;
+            this.lbl3.Text = "Enter Basic Salary";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(306, 49);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(147, 26);
+            this.txtId.TabIndex = 3;
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Location = new System.Drawing.Point(306, 215);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(147, 26);
+            this.txtSalary.TabIndex = 4;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(306, 138);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(147, 26);
+            this.txtName.TabIndex = 5;
+            // 
+            // btnBinaryWrite
+            // 
+            this.btnBinaryWrite.Location = new System.Drawing.Point(225, 323);
+            this.btnBinaryWrite.Name = "btnBinaryWrite";
+            this.btnBinaryWrite.Size = new System.Drawing.Size(121, 30);
+            this.btnBinaryWrite.TabIndex = 6;
+            this.btnBinaryWrite.Text = "Binary Write";
+            this.btnBinaryWrite.UseVisualStyleBackColor = true;
+            this.btnBinaryWrite.Click += new System.EventHandler(this.btnBinaryWrite_Click);
+            // 
+            // btnBinaryRead
+            // 
+            this.btnBinaryRead.Location = new System.Drawing.Point(378, 323);
+            this.btnBinaryRead.Name = "btnBinaryRead";
+            this.btnBinaryRead.Size = new System.Drawing.Size(121, 30);
+            this.btnBinaryRead.TabIndex = 7;
+            this.btnBinaryRead.Text = "Binary Read";
+            this.btnBinaryRead.UseVisualStyleBackColor = true;
+            this.btnBinaryRead.Click += new System.EventHandler(this.btnBinaryRead_Click);
+            // 
+            // btnCreateFolder
+            // 
+            this.btnCreateFolder.Location = new System.Drawing.Point(516, 55);
+            this.btnCreateFolder.Name = "btnCreateFolder";
+            this.btnCreateFolder.Size = new System.Drawing.Size(121, 30);
+            this.btnCreateFolder.TabIndex = 8;
+            this.btnCreateFolder.Text = "Create Folder";
+            this.btnCreateFolder.UseVisualStyleBackColor = true;
+            this.btnCreateFolder.Click += new System.EventHandler(this.btnCreateFolder_Click);
+            // 
+            // btnCreateFile
+            // 
+            this.btnCreateFile.Location = new System.Drawing.Point(667, 55);
+            this.btnCreateFile.Name = "btnCreateFile";
+            this.btnCreateFile.Size = new System.Drawing.Size(121, 30);
+            this.btnCreateFile.TabIndex = 9;
+            this.btnCreateFile.Text = "Create File";
+            this.btnCreateFile.UseVisualStyleBackColor = true;
+            this.btnCreateFile.Click += new System.EventHandler(this.btnCreateFile_Click);
+            // 
+            // btnDeleteFile
+            // 
+            this.btnDeleteFile.Location = new System.Drawing.Point(663, 120);
+            this.btnDeleteFile.Name = "btnDeleteFile";
+            this.btnDeleteFile.Size = new System.Drawing.Size(124, 30);
+            this.btnDeleteFile.TabIndex = 10;
+            this.btnDeleteFile.Text = "Delete File";
+            this.btnDeleteFile.UseVisualStyleBackColor = true;
+            this.btnDeleteFile.Click += new System.EventHandler(this.btnDeleteFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDeleteFile);
+            this.Controls.Add(this.btnCreateFile);
+            this.Controls.Add(this.btnCreateFolder);
+            this.Controls.Add(this.btnBinaryRead);
+            this.Controls.Add(this.btnBinaryWrite);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnBinaryWrite;
+        private System.Windows.Forms.Button btnBinaryRead;
+        private System.Windows.Forms.Button btnCreateFolder;
+        private System.Windows.Forms.Button btnCreateFile;
+        private System.Windows.Forms.Button btnDeleteFile;
+    }
+}
+
---
+++ IODemo/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace IODemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreateFolder_Click(object sender, EventArgs e)
+        {
+            string path = @"C:\SkillMineDoc";
+            try
+            {
+                if(Directory.Exists(path))
+                {
+                    MessageBox.Show("Directory id already exists");
+
+                }
+                else
+                {
+                    Directory.CreateDirectory(path);
+                    MessageBox.Show("Directory Created.");
+                }
+            }
+            catch(Exception ex) 
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnCreateFile_Click(object sender, EventArgs e)
+        {
+            string path = @"C:\SkillMineDoc\TextDemo.txt";
+            try
+            {
+                if(File.Exists(path))
+                {
+                    MessageBox.Show("File is already exists.");
+
+                }
+                else
+                {
+                    File.Create(path);
+                    MessageBox.Show("File is created.");
+                }
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnDeleteFile_Click(object sender, EventArgs e)
+        {
+            string path = @"C:\SkillMineDoc\TextDemo.txt";
+            try
+            {
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                    MessageBox.Show("File has been removed");
+                }
+                else
+                {
+                    MessageBox.Show("File not found");
+                }
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnBinaryWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FileStream fs=new FileStream(@"C:\SkillMineDoc\emp.dat",FileMode.Create,FileAccess.Write);  
+                BinaryWriter bw=new BinaryWriter(fs);   
+                bw.Write(Convert.ToInt32(txtId.Text));
+                bw.Write(txtName.Text);
+                bw.Write(Convert.ToDouble(txtSalary.Text));
+                bw.Close();
+                fs.Close();
+                MessageBox.Show("Data Saved to File....");
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);    
+            }
+        }
+
+        private void btnBinaryRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FileStream fs = new FileStream(@"C:\SkillMineDoc\emp.dat", FileMode.Open, FileAccess.Read);
+                BinaryReader br = new BinaryReader(fs);
+                txtId.Text = br.ReadInt32().ToString();
+                txtName.Text = br.ReadString();
+                txtSalary.Text = br.ReadDouble().ToString();
+                br.Close();
+                fs.Close();
+            }catch(Exception ex) 
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
